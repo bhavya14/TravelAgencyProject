9c98517a00c7a5f606b48668dcd443d5bfeebd21
bhavya14 <bhavya.gupta97@gmail.com>
2018-04-07 23:36:07 +0530

    remaining merge heads
    Signed-off-by: bhavya14 <bhavya.gupta97@gmail.com>

--- server.js
+++ server.js
@@ -3,37 +3,20 @@ const app = express();
 const ejs = require('ejs')
 var bodyParser = require('body-parser');
 var path = require('path');
-<<<<<<< HEAD
-var db = require('./db');
-=======
 var db = require('./db/db');
 var bdb=require('./db/booking_db');
 var udb=require('./db/user_db');
->>>>>>> 24b8a8058ce72774da56d6d51dba6191ee1e6a73
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-<<<<<<< HEAD
-app.post("/profile" ,function(req,res){
-    console.log(req.body.password);
-    db.display(req.body.username,'*',req.body.password,function(data){
-        // if(data!=null) {
-        //     res.render('pages/profile', {
-        //         name: data[0].first_name,
-        //         last_name : data[0].last_name
-        //     });
-        // }
-=======
 app.post("/profile" ,function(req,res){ // User Page
     console.log(req.body.password);
 
 
     db.display(req.body.username,'*',req.body.password,function(data){
-
->>>>>>> 24b8a8058ce72774da56d6d51dba6191ee1e6a73
         console.log("Data is");
         console.log(data);
         if(data==undefined)
@@ -55,36 +38,9 @@ app.post("/profile" ,function(req,res){ // User Page
                 last_name : data[0].last_name
             });
         }
-<<<<<<< HEAD
-=======
-
->>>>>>> 24b8a8058ce72774da56d6d51dba6191ee1e6a73
     })
 
 })
-
-<<<<<<< HEAD
-app.post('/booking',function (req,res) {
-
-//    bdb.createtable();
-    var data=req;
-    res.render('pages/booking', {
-    });
-})
-
-app.post('/signup',function (req,res) {
-    db.add(req.body.username2,req.body.password2,function (data) {
-        res.render('pages/profile', {
-
-        });
-
-    })
-})
-
-app.get("/",function(req,res){
-    res.render('pages/index')
-})
-=======
 
 app.post('/booking',function (req,res) {
 
@@ -155,7 +111,6 @@ app.get("/",function(req,res){
 
     res.render('pages/index')
 });
->>>>>>> 24b8a8058ce72774da56d6d51dba6191ee1e6a73
 app.listen(3000, function() {
     console.log("Running on 3000");
     db.connect();
